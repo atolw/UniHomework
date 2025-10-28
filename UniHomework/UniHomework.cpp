e8420acbcb1a037b4c5bb5dfb5b4d0bd66fa361e
atolw <ztolik228@gmail.com>
2025-10-28 09:34:11 +0200

    Refactor main function for range calculation
    Updated the program to calculate and display values of y based on the logarithm and power of x in a specified range.

--- UniHomework/UniHomework.cpp
+++ UniHomework/UniHomework.cpp
@@ -1,21 +1,23 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <windows.h>
+
+
 using namespace std;
 int main()
 {
-    // Задано три цілих числа: A, B, C. Перевірити істинність висловлювання: «Хоча б одне з чисел A, B, C позитивне». 
-
-    int a, b, c;
-
-    cout << boolalpha << "Enter a, b, c in order: ";
-    cin >> a >> b >> c;
-    cout << "The statement is ";
-        (a > 0 || b > 0 || c >> 0) ? cout << true : cout << false;
-    cout << endl;
-
-    cout << "The statement is " << (a > 0 || b > 0 || c >> 0);
-
-    // 67
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    
+	float a = 2, b = 3, y, step = 0.1;	 
+	cout << "range [" << a << "; " << b << "]\nx\t y\t " << endl;
+	while (a <= b)
+	{
+		y = pow(log10(a), 2) + pow(a, 2) + 2;
+		cout << "x = " << showpoint << setprecision(2) << a << "\t y = " << setprecision(4) << y << endl;
+		a += step;
+	}
 }
+
+
